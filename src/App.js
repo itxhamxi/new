4b71f0b3f18d557ca129a3cf034845049ac82310
itxhamxi <khizarhayat1888@gmail.com>
2020-09-05 01:09:24 +0500

    Sddfdfadfdf

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Header from './components/header';
+import Newheader from './components/Newheader'
+function App() {
+  return (
+    <div>
+      
+      <Newheader/>
+     
+    </div>
+  );
+}
+
+export default App;
